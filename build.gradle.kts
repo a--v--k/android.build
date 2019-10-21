0414ea00ddaa9c4df309f45e693c922ad86b2d71
Alexander Kasatkin <alexander.v.kasatkin@gmail.com>
2019-10-21 11:22:57 +0300

    [012] Switch version to 3.5.0-rc-09.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jetbrains.kotlin.konan.properties.loadProperties
 
 group = "org.ak2"
-version = "3.5.0-rc-08"
+version = "3.5.0-rc-09"
 
 require(File("local.properties").exists()) { """
 The following properties must be stored in local.properties:
