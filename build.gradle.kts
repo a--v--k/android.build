1ede7b54876179032ffbbde4a4c5a6cad95d5fe6
Alexander Kasatkin <alexander.v.kasatkin@gmail.com>
2020-10-30 23:17:11 +0300

    [023] Switch version to 4.1.0-beta-18.

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jetbrains.kotlin.konan.properties.loadProperties
 
 group = "org.ak2"
-version = "4.1.0-beta-17"
+version = "4.1.0-beta-18"
 
 require(File("local.properties").exists()) { """
 The following properties must be stored in local.properties:
