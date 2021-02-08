4e0fb93e2fe760111d9b431feb5272b6e1ca5cb3
Alexander Kasatkin <alexander.v.kasatkin@gmail.com>
2021-02-09 00:55:11 +0300

    [027] Tests improved.

--- src/test/resources/002-simple-app/build.gradle.kts
+++ src/test/resources/002-simple-app/build.gradle.kts
@@ -10,6 +10,8 @@ repositories {
 
 androidApp {
 
+    additionalManifests = project.fileTree("src/main/manifests").apply { include("*/AndroidManifest.xml") }
+
     dependsOn {
         compileOnly += library("androidx.annotation:annotation:1.1.0")
         compileOnly += library("org.jetbrains:annotations:13.0")
