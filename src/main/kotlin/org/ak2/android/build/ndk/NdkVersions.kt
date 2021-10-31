0196f439f0fb335968a30b5ed1fc9bfcd8cd8d90
Alexander Kasatkin <alexander.v.kasatkin@gmail.com>
2021-10-31 13:34:58 +0300

    [033] NDK versions updated.

--- src/main/kotlin/org/ak2/android/build/ndk/NdkVersions.kt
+++ src/main/kotlin/org/ak2/android/build/ndk/NdkVersions.kt
@@ -18,12 +18,29 @@ package org.ak2.android.build.ndk
 
 sealed class NdkVersion(val version: String) {
 
-    object R22 : NdkVersion("22.0.7026061")
-    object R21 : NdkVersion("21.3.6528147")
-    object R20 : NdkVersion("20.1.5948944")
-    object R19 : NdkVersion("19.2.5345600")
-    object R18 : NdkVersion("18.1.5063045")
-    object R17 : NdkVersion("17.2.4988734")
-    object R16 : NdkVersion("16.1.4479499")
+    constructor(version : NdkVersion) : this(version.version)
+
+    object Latest : NdkVersion(R23)
+
+    object R23 : NdkVersion(R23_1)
+    object R22 : NdkVersion(R22_1)
+    object R21 : NdkVersion(R21_4)
+    object R20 : NdkVersion(R20_1)
+
+    object R23_1: NdkVersion("23.1.7779620")
+    object R23_0: NdkVersion("23.0.7599659")
+
+    object R22_1 : NdkVersion("22.1.7171370")
+    object R22_0 : NdkVersion("22.0.7026061")
+
+    object R21_4 : NdkVersion("21.4.7026061")
+    object R21_3 : NdkVersion("21.3.6528147")
+
+    object R20_1 : NdkVersion("20.1.5948944")
+    object R19_2 : NdkVersion("19.2.5345600")
+    object R18_1 : NdkVersion("18.1.5063045")
+    object R17_2 : NdkVersion("17.2.4988734")
+    object R16_1 : NdkVersion("16.1.4479499")
+
     class Custom(version: String) : NdkVersion(version)
 }
